afd6fcfd89b691d6199faf0c0ea6c5438e9f5746
root <root@localhost.localdomain>
2026-03-31 21:45:02 +0800

    fix: 简化 Application，移除 Worker 相关代码

--- app/src/main/java/com/plantlog/app/PlantLogApplication.kt
+++ app/src/main/java/com/plantlog/app/PlantLogApplication.kt
@@ -2,25 +2,12 @@ package com.plantlog.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import dagger.hilt.android.EntryPointAccessors
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
-import javax.inject.Inject
 
 /**
  * Application 入口类
  */
 @HiltAndroidApp
-class PlantLogApplication : Application(), Configuration.Provider {
-    
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
-    
-    override val workManagerConfiguration: Configuration
-        get() = Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
-    
+class PlantLogApplication : Application() {
     override fun onCreate() {
         super.onCreate()
     }
