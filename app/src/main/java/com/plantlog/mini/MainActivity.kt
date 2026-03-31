a0aa00ce0c1377746cc7dbebdc0d963c666ce0cf
root <root@localhost.localdomain>
2026-03-31 20:09:29 +0800

    fix: 修复 MainActivity.kt import 语法错误

--- app/src/main/java/com/plantlog/mini/MainActivity.kt
+++ app/src/main/java/com/plantlog/mini/MainActivity.kt
@@ -2,7 +2,7 @@ package com.plantlog.mini
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent {
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
