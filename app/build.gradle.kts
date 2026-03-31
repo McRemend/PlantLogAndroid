a717808255f10b6036312458e15d3b845d4c0754
root <root@localhost.localdomain>
2026-03-31 20:41:27 +0800

    fix: 添加 hilt-work 依赖支持 Worker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,8 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.48.1")
     ksp("com.google.dagger:hilt-android-compiler:2.48.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation("androidx.hilt:hilt-work:1.1.0")
+    ksp("androidx.hilt:hilt-compiler:1.1.0")
 
     // Coil (Image Loading)
     implementation("io.coil-kt:coil-compose:2.6.0")
