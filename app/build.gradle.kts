a9828241d441f1793471aae34d8e36789cdf1c45
root <root@localhost.localdomain>
2026-03-31 23:38:12 +0800

    fix: 升级 Hilt 到 2.51.1，移除 hilt-work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,11 +77,9 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.48.1")
-    ksp("com.google.dagger:hilt-android-compiler:2.48.1")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-    implementation("androidx.hilt:hilt-work:1.1.0")
-    ksp("androidx.hilt:hilt-compiler:1.1.0")
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // Coil (Image Loading)
     implementation("io.coil-kt:coil-compose:2.6.0")
