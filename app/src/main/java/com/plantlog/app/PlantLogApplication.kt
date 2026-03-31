6d4e1e9d4000fa9357b84003f48ee3a662f9db92
root <root@localhost.localdomain>
2026-04-01 03:09:55 +0800

    fix: DatabaseModule 直接注入 PlantLogApplication，移除 AppModule

--- app/src/main/java/com/plantlog/app/PlantLogApplication.kt
+++ app/src/main/java/com/plantlog/app/PlantLogApplication.kt
@@ -1,14 +1,7 @@
 package com.plantlog.app
 
 import android.app.Application
-import android.content.Context
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
-import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 /**
  * Application 入口类
@@ -19,14 +12,4 @@ class PlantLogApplication : Application() {
     override fun onCreate() {
         super.onCreate()
     }
-}
-
-@Module
-@InstallIn(SingletonComponent::class)
-object AppModule {
-    
-    @Provides
-    @Singleton
-    @ApplicationContext
-    fun provideApplicationContext(application: PlantLogApplication): Context = application
 }
