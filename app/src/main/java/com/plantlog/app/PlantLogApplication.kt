35576726ae13edbaeb24aa5c024ed8e8fda5aa8f
root <root@localhost.localdomain>
2026-04-01 00:18:00 +0800

    fix: 清理构建缓存

--- app/src/main/java/com/plantlog/app/PlantLogApplication.kt
+++ app/src/main/java/com/plantlog/app/PlantLogApplication.kt
@@ -1,14 +1,7 @@
 package com.plantlog.app
 
 import android.app.Application
-import android.content.Context
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
-import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 /**
  * Application 入口类
@@ -19,13 +12,4 @@ class PlantLogApplication : Application() {
     override fun onCreate() {
         super.onCreate()
     }
-}
-
-@Module
-@InstallIn(SingletonComponent::class)
-object AppModule {
-    
-    @Provides
-    @Singleton
-    fun provideContext(application: PlantLogApplication): Context = application
 }
