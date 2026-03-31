18b24b18adaddb238f3048d211cfcb29c68f33c2
root <root@localhost.localdomain>
2026-03-31 21:18:31 +0800

    fix: Application 实现 Configuration.Provider 支持 Worker

--- app/src/main/java/com/plantlog/app/PlantLogApplication.kt
+++ app/src/main/java/com/plantlog/app/PlantLogApplication.kt
@@ -2,12 +2,25 @@ package com.plantlog.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.EntryPointAccessors
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import javax.inject.Inject
 
 /**
  * Application 入口类
  */
 @HiltAndroidApp
-class PlantLogApplication : Application() {
+class PlantLogApplication : Application(), Configuration.Provider {
+    
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+    
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+    
     override fun onCreate() {
         super.onCreate()
     }
